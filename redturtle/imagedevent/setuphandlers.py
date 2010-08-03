3f38923deb84e9166e8d66addd5c84ed80e3588a
keul <luca@keul.it>
2010-08-03 07:51:10 +0000

    Removed old deprecated code

--- redturtle/imagedevent/setuphandlers.py
+++ redturtle/imagedevent/setuphandlers.py
@@ -8,25 +8,6 @@ def setupVarious(context):
     
     if context.readDataFile('redturtle.imagedevent_various.txt') is None: 
         return
-    
-    # Disabled: have the old portal_type in the types_tool duplicate "Event" choice in some places
-    #backupATEvent(portal)
 
-def backupATEvent(portal):
-    """Make a backup of ATEvent before the types.xml step overwrite it"""
-    portal_types = portal.portal_types
-    try:
-        oldevent_fti = getattr(portal_types, 'oldATEvent')
-    except AttributeError:
-        oldevent_fti = None
-    if oldevent_fti:
-        print "Doing nothing: ATEvent already disabled"
-        return
-    
-    portal_types.manage_renameObject('Event', 'oldATEvent')
-    oldevent_fti = getattr(portal_types, 'oldATEvent')
-    
-    oldevent_fti.global_allow = False
-    print "Disabled Plone original ATEvent"
 
     
